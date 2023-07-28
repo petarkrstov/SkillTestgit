27a2899c1b840ad2aeda039925dac7f8cf3a53ff
Petar Krstov <74118176+petarkrstov@users.noreply.github.com>
2023-07-28 10:52:52 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+#define LED_PIN 13
+#define BLINK_DELAY 100
+
+int n = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_PIN ,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(BLINK_DELAY);
+  digitalWrite(LED_PIN, LOW);
+  delay(BLINK_DELAY);
+  }
+//comment
