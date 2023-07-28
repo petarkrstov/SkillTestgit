5fb80f2365d74d8df93f1b8c1f9da4c429338075
Petar Krstov <74118176+petarkrstov@users.noreply.github.com>
2023-07-28 14:42:42 +0200

    Added 4 tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,96 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
 
-#define LED_PIN 13
-#define BLINK_DELAY 200
+void Task_Print1(void *parameter);
+void Task_Print2(void *parameter);
+void Task_Led(void *parameter);
+void Task_Time(void *parameter);
 
-int n = 0;
+TaskHandle_t Task_Handle1;
+TaskHandle_t Task_Handle2;
+TaskHandle_t Task_HandleLed;
+TaskHandle_t Task_HandleTime;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  pinMode(LED_PIN ,OUTPUT);
+  // Ime na funkcija, Name of task, Stack memmory, parameter, priority, &TaskHandle_t name
+  xTaskCreate(Task_Print1, "Task1", 100, NULL, 1, &Task_Handle1);
+  xTaskCreate(Task_Print2, "Task2", 100, NULL, 1, &Task_Handle2);
+  xTaskCreate(Task_Led, "TaskLed", 100, NULL, 1, &Task_HandleLed);
+  xTaskCreate(Task_Time, "TaskTime", 100, NULL, 1, &Task_HandleTime);
+  pinMode(13, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(BLINK_DELAY);
-  digitalWrite(LED_PIN, LOW);
-  delay(BLINK_DELAY);
+void loop()
+{
+  // No code here!!!
+  // It will print on serial whenever CPU is in idle
+  // Serial.println(F("Loop function"));
+  // delay(50);
+}
+
+void Task_Print1(void *parameter)
+{
+  (void)parameter;
+  // Loop for this task
+  while (1)
+  {
+    Serial.println("Task1");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
-//comment
+}
+
+void Task_Print2(void *parameter)
+{
+  (void)parameter;
+  // Loop for this task
+  while (1)
+  {
+    Serial.println("Task2");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void Task_Led(void *parameter)
+{
+  (void)parameter;
+  // Loop for this task
+  while (1)
+  {
+    digitalWrite(13, HIGH);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    digitalWrite(13, LOW);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+void Task_Time(void *parameter)
+{
+  (void)parameter;
+  // Loop for this task
+  while (1)
+  {
+    unsigned long currentTime = millis(); // Get current time in milliseconds
+
+    // Convert milliseconds to seconds, minutes, and hours
+    unsigned long seconds = currentTime / 1000;
+    unsigned long minutes = seconds / 60;
+    unsigned long hours = minutes / 60;
+
+    // Calculate remaining seconds and minutes
+    seconds = seconds % 60;
+    minutes = minutes % 60;
+
+    // Print the elapsed time to the serial console
+    Serial.print("Time Elapsed: ");
+    Serial.print(hours);
+    Serial.print("h ");
+    Serial.print(minutes);
+    Serial.print("m ");
+    Serial.print(seconds);
+    Serial.println("s");
+
+    vTaskDelay(5000 / portTICK_PERIOD_MS); // Wait for 1 second before printing again
+  }
+}
